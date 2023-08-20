b5d0586e5bc9f99d3be81b0adeac25a9e7ec9d46
bspraveen <billasujith77@gmail.com>
2023-08-20 21:07:08 +0530

    Authentication with only username and password check

---
+++ app.js
@@ -0,0 +1,74 @@
+//jshint esversion:6
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser
+    .save()
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    })
+    .then(() => {
+      res.render("secrets");
+    });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username, password: password })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    })
+    .then((foundUser) => {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      } else {
+        console.log("User not registered");
+      }
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
